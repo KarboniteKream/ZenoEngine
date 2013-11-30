1d15395e0022fbd65523e71b33f466bafdcd736a
Klemen Košir <klemen913@gmail.com>
2013-11-30 14:12:20 +0100

    Revert endianness checking.

--- texture.c
+++ texture.c
@@ -53,21 +53,7 @@ void loadTexture(Texture *texture, const char *filename)
 
 	if(texture->Width != texture->TexWidth || texture->Height != texture->TexHeight)
 	{
-		Uint32 rMask, gMask, bMask, aMask;
-
-		#if SDL_BYTEORDER == SDL_BIG_ENDIAN
-			rMask = 0xFF000000;
-			gMask = 0x00FF0000;
-			bMask = 0x0000FF00;
-			aMask = 0x000000FF;
-		#else
-			rMask = 0x000000FF;
-			gMask = 0x0000FF00;
-			bMask = 0x00FF0000;
-			aMask = 0xFF000000;
-		#endif
-
-		SDL_Surface *paddedImage = SDL_CreateRGBSurface(0, texture->TexWidth, texture->TexHeight, 32, rMask, gMask, bMask, aMask);
+		SDL_Surface *paddedImage = SDL_CreateRGBSurface(0, texture->TexWidth, texture->TexHeight, 32, 0x000000FF, 0x0000FF00, 0x00FF0000, 0xFF000000);
 
 		SDL_SetSurfaceAlphaMod(image, 255);
 		SDL_SetSurfaceBlendMode(image, SDL_BLENDMODE_NONE);
